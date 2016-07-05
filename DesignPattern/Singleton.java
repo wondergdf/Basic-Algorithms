5c7d760c6366a7d526c8332037b1e0c8f960e3eb
xliu050 <Xiao.B.Liu@alcatel-sbell.com.cn>
2016-07-05 23:23:40 +0800

    Add design pattern branch for Basic-Algorithms. And the first pattern is Singleton which implements by five ways.

---
+++ DesignPattern/Singleton.java
@@ -0,0 +1,87 @@
+package DesignPattern;
+
+import java.io.Serializable;
+
+class LazyMan {
+	private static LazyMan instance;
+
+	private LazyMan() {
+		if (instance != null) {// avoid reflection attack
+			throw new ExceptionInInitializerError("Instance already exist.");
+		}
+	}
+
+	public static synchronized LazyMan getInstance() {
+		if (instance == null) {
+			instance = new LazyMan();
+		}
+		return instance;
+	}
+}
+
+class HungryMan {
+	private static HungryMan instance = new HungryMan();
+
+	private HungryMan() {
+
+	}
+
+	public static HungryMan getInstance() {
+		return instance;
+	}
+}
+
+class StaticClass {
+	private static class InstanceHolder {
+		private static final StaticClass instance = new StaticClass();
+	}
+
+	private StaticClass() {
+
+	}
+
+	public static final StaticClass getInstance() {
+		return InstanceHolder.instance;
+
+	}
+}
+
+class DoubleCheck implements Serializable {
+	private static DoubleCheck Instance;
+
+	private DoubleCheck() {
+
+	}
+
+	public static DoubleCheck getInstance() {
+		if (Instance == null) {
+			synchronized (DoubleCheck.class) {
+				if (Instance == null) {
+					Instance = new DoubleCheck();
+				}
+			}
+		}
+		return Instance;
+	}
+
+	private Object readResolve() {// avoid serialization attack
+		return Instance;
+	}
+}
+
+enum EnumSingleton {
+	Instance;
+
+	public void doSomething() {
+
+	}
+
+}
+
+public class Singleton {
+	public static void main(String[] args) {
+		
+		EnumSingleton instanc = EnumSingleton.Instance;
+
+	}
+}
