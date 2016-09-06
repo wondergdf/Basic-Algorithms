c75a8efcbfce89db3af5462672408356c3a41198
wondergdf <wondergdflx@hotmail.com>
2016-09-06 09:29:10 +0800

    Update Heap_Sort.java
    amend the syntax of the generic method,"Comparable<Key>[ ]" is not correct , the generic array is not valid .

--- Basic_Sort/Heap_Sort.java
+++ Basic_Sort/Heap_Sort.java
@@ -4,18 +4,18 @@ import java.util.Comparator;
 public class Heap_Sort<Key extends Comparable<Key>> {
 
 	
-	private static boolean less(Comparable<Key>[] data ,int i, int j) {
+	private static boolean less(Comparable[] data ,int i, int j) {
 		return data[i].compareTo(data[j]) < 0;
 	}
 
-	private static void exchange(Comparable<Key>[] data,int i, int j) {
+	private static void exchange(Comparable[] data,int i, int j) {
 		Key temp = data[i];
 		data[i] = data[j];
 		data[j] = temp;
 	}
 
 	
-	private static void sink(Comparable<Key>[] data, int root,int end) {
+	private static void sink(Comparable[] data, int root,int end) {
 		while ((2 * root + 1) <= end) {
 			int child = 2 * root +1;// the left child 
 			if (child +1<= end && less(child, j + 1)) {
@@ -30,16 +30,16 @@ public class Heap_Sort<Key extends Comparable<Key>> {
 	}
 
 	
-	public static void heap_Sort(Comparable<Key>[] data){
+	public static void heap_Sort(Comparable[] data){
 
 		int length = data.length;
 		int last_patrent = (data.length-2)/2; 
 
 		for(int k = last_patrent ; k >=0 ; k--){
-          sink(data,k,length-1);// step 1 : heapify 
+                  sink(data,k,length-1);// step 1 : heapify 
 		}
          
-        int end = length -1;
+                int end = length -1;
 		while(end > 0){
 			exchange(data,0, end);// step 2: exchange the root node(max value) with the last leaf node ,the biggest leaf save to the data[end] position.
 			sink(data,0,end -1);//step 3: heapify the rest nodes (every time find the max value from the rest nodes {M top K problem})
@@ -48,7 +48,7 @@ public class Heap_Sort<Key extends Comparable<Key>> {
 	}
 	
 	@Deprecated
-	public static void print(Comparable<Key>[] data) {
+	public static void print(Comparable[] data) {
 		for (int i = 1; i<= N; i++) {
 			System.out.print(data[i].toString()+" ");
 		}
